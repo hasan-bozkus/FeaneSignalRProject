2913ece2e3bd2a869d45d7178e3284dcde18d96e
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-07 14:09:29 +0300

    Asp.Net Core Api SignalR ile QR Kodlu Sipariş Yönetimi
    Bölüm 1: Giriş ve Entitylerin Yazılması
    Giriş,
    SignalR Nedir,
    Projenin Oluşturulması,
    Entitylerin Tamamlanması,
    Paketlerin Kurulması,
    Bağlantı Sınfının Yazılması,
    Interfacelerin Yazılması,
    Data Access Katmanının Tamamlanması,
    Business Abstract Serviceler,
    Manager Sınıflarının Yazılması,
    Küçük Bir Bilgi,
    Erişim Belirleyiciler,
    Dtoların Tanımlanması 1,
    Dtoların Tanımlanması 2

---
+++ SignalR.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly SignalRContext _signalRContext;
+
+        public GenericRepository(SignalRContext signalRContext)
+        {
+            _signalRContext = signalRContext;
+        }
+
+        public void Add(T entity)
+        {
+            _signalRContext.Add(entity);
+            _signalRContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _signalRContext.Remove(entity);
+            _signalRContext.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            return _signalRContext.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return _signalRContext.Set<T>().ToList();
+        }
+
+        public void Update(T entity)
+        {
+            _signalRContext.Update(entity);
+            _signalRContext.SaveChanges();
+        }
+    }
+}
