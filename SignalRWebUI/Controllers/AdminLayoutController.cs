a2cb3f9a11316a9deef4cfb902798b422eb1e1a0
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-17 12:49:13 +0300

    Asp.Net Core Api SignalR ile QR Kodlu Sipariş Yönetimi
    Bölüm 4: Tema Hazırlığı
    Layout Hazırlığı,
    Header Layout,
    Navbar Layout,
    Sidebar Layout,
    Script Layout

---
+++ SignalRWebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+    }
+}
