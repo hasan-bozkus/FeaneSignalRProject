d936aab8f19047724077bbad29f272268f305fc2
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-25 21:35:42 +0300

    Asp.Net Core Api SignalR ile QR Kodlu Sipariş Yönetimi
    Bölüm 8: SignalR Kütüphanesi
    Client Side Library ile SignalR Paketleri,
    SignalR Hub Sınıfı,
    Cors Politikası,
    Endpoint Yazılması,
    Client Connected Kodları,
    Client'ın Layouta taşınması,
    Kategori Sayılarını Getirme Hazırlığı,
    Kategori Sayılarını Geirme,
    Set Interval Metodu

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -3,11 +3,24 @@ using SignalR.BusinessLayer.Concrete;
 using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.DataAccessLayer.EntityFramework;
+using SignalRApi.Hubs;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR();
+
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
@@ -52,10 +65,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapHub<SignalRHub>("/signalrhub");
+
 app.Run();
+
+//localhost://1234/swagger/category/index
+//localhost://1234/signalrhub
