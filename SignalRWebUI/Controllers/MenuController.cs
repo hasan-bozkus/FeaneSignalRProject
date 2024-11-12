23a0ff8365573916148ba262d9ead0b6bc3f5aa1
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-11-12 21:42:38 +0300

    Asp.Net Core Api SignalR ile QR Kodlu Sipariş Yönetimi
    Bölüm 14: Menü İşlemleri ve Ajax
    Menü Sayfası,
    Menüde Ürünlerin Listelenmesi,
    Ajax ile Tıklana Ürün ID Değerini Alma,
    Sweet Alert

--- SignalRWebUI/Controllers/MenuController.cs
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -3,20 +3,22 @@ using Newtonsoft.Json;
 using SignalRWebUI.Dtos.CategoryDtos;
 using SignalRWebUI.Dtos.ProductDtos;
 using SignalRWebUI.Models.Products;
+using System.Net.Http;
 
-namespace SignalRWebUI.ViewComponents.DefaultComponents
+namespace SignalRWebUI.Controllers
 {
-    public class _DefaultOurMenuComponentPartial : ViewComponent
+    public class MenuController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _DefaultOurMenuComponentPartial(IHttpClientFactory httpClientFactory)
+        public MenuController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IActionResult> Index()
         {
+
             var client = _httpClientFactory.CreateClient();
             var responseProductMessage = await client.GetAsync("https://localhost:44303/api/Product/ProdctListWithCategory");
             var productsWithCategory = new List<ResultProductWithCategoryDto>();
---
+++ SignalRWebUI/Models/Products/ProductandCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SignalRWebUI.Dtos.CategoryDtos;
+using SignalRWebUI.Dtos.ProductDtos;
+
+namespace SignalRWebUI.Models.Products
+{
+    public class ProductandCategoryViewModel
+    {
+        public List<ResultProductWithCategoryDto> Products { get; set; }
+        public List<ResultCategoryDto> Categories { get; set; }
+    }
+}
